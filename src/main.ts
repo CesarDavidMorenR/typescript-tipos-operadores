4ebf22c87c7554d6fd83a9bd8519c73b12f99caa
DavidMorenoDev <cesardavidmor@gmail.com>
2023-09-16 21:46:10 +0200

    Project Done

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,19 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+// Definición de variables con tipos
+const costoTotalCena: number = 120;
+const costoBebidasIncluidas: number = 18;
+const numeroDeAmigos: number = 6;
+
+// Calcular el costo total de la cena sin las bebidas
+const costoCenaSinBebidas: number = costoTotalCena - costoBebidasIncluidas;
+
+// Calcular el costo que debe pagar cada comensal
+const costoPorComensal: number = costoCenaSinBebidas / numeroDeAmigos;
+
+console.log(
+  `El costo total de la cena sin las bebidas es de ${costoCenaSinBebidas} €.`
+);
+console.log(
+  `Cada uno de los ${numeroDeAmigos} amigos debe pagar ${costoPorComensal} € para dividir los costos de manera equitativa.`
+);
